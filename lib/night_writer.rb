b412311a2e0543020e025063e262f43c613fffd4
Chaz Simons <chaz.simons@gmail.com>
2021-09-25 12:04:54 -0700

    Update dictionary, rough draft of methods for class to convert text to braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,19 +2,54 @@ require './lib/dictionary'
 require './lib/file_reader'
 require './lib/file_writer'
 
-class NightWriter
+dictionary = Dictionary.new
 
-  def initialize
-    @dictionary = Dictionary.new
-    @reader = FileReader.new
-    @writer = FileWriter.new
-  end
+inputs = ARGV
 
-  def created_new_file
-    puts "Created '#{@writer}' containing #{@reader.length} characters"
-  end
+original_message = File.read(inputs[0])
 
-  def breakdown
-    @reader.new.split(//)
+puts "Created '#{inputs[1]}' containing #{original_message.length} characters"
+
+array = original_message.split(//)
+
+top_line = []
+middle_line = []
+bottom_line = []
+
+braille = array.each do |character|
+  if character == " "
+    top_line << "  "
+    middle_line << "  "
+    bottom_line << "  "
+  else
+    dictionary.top.keys.include?(character)
+      top_line    << dictionary.top[character]
+    dictionary.middle.keys.include?(character)
+      middle_line << dictionary.middle[character]
+    dictionary.bottom.keys.include?(character)
+      bottom_line << dictionary.bottom[character]
   end
+  top_line.join
+  middle_line.join
+  bottom_line.join
 end
+
+
+translated_message = File.write("#{inputs[1]}", braille)#this is where the braille message goes)
+
+# class NightWriter
+#
+#   def initialize
+#     @dictionary = Dictionary.new
+#     @reader = FileReader.new
+#     @writer = FileWriter.new
+#   end
+#
+#   def created_new_file
+#     puts "Created '#{@writer}' containing #{@reader.length} characters"
+#   end
+#
+#   def breakdown
+#     @reader.new.split(//)
+#   end
+# end
